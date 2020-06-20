4d9b440257c4562eae258f4e14ac21a337730ea8
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-20 06:58:13 +0530

    Mobile number

--- userreg.sh
+++ userreg.sh
@@ -3,7 +3,7 @@ echo "Welcome to User Registration"
 
 Name_pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
 Email_pattern="^[a-zA-z0-9]+(\.[a-zA-z0-9+])*@([a-z0-9]+([a-z0-9]*)\.)+[a-z]{2,4}$"
-
+mobile_pattern="^[1-9]{2}[[:space:]]{1}[0-9]{10}$"
 function validate() {
 
 	read -p "Enter:" value
@@ -29,11 +29,16 @@ function Email() {
         echo "Enter your email"
         validate $Email_pattern
         }
+function mobile() {
+	echo "Enter your mobile"
+	validate $mobile_pattern
+}
 
 function patternEnter() {
 	FirstName
 	LirstName
 	Email
+	mobile
 }
 
 patternEnter
