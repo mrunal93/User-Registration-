e358c206fd39c125ef544270b40080aa29d0f0c8
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-20 06:30:09 +0530

    Last name

--- userreg.sh
+++ userreg.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to User Registration"
 
 read -p "Enter your first name:" name
+read  -p "Enter your Last name:" Lname
 pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
 
 if [[ $name =~ $pattern ]]
@@ -11,4 +12,12 @@ else
 	echo "Invalid"
 
 fi
+if [[ $Lname =~ $pattern ]]
+then
+        echo "Valid"
+else
+        echo "Invalid"
+
+fi
+
 
