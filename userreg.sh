bbcc996cfc1c4d6731ffabbe1360eabe1df459d7
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-20 06:18:06 +0530

    First name

--- userreg.sh
+++ userreg.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "Welcome to User Registration"
 
+read -p "Enter your first name:" name
+pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $name =~ $pattern ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
+
